8208658b196fdb8b24d9696de83d452f8577235b
Hari Krishna <harikrishnathoka@gmail.com>
2026-01-16 18:37:46 +0530

    resolving build issues

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   babel: {
     plugins: [
@@ -11,34 +13,46 @@ module.exports = {
   },
   webpack: {
     configure: (webpackConfig) => {
+      // Find the oneOf array that contains the babel-loader rule
       const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf);
 
       if (oneOfRule) {
-        const babelLoader = oneOfRule.oneOf.find(
-          (rule) => rule.loader && rule.loader.includes('babel-loader')
-        );
-
-        if (babelLoader) {
-          // Remove the exclude for @mui and @emotion packages
-          const originalExclude = babelLoader.exclude;
-          babelLoader.exclude = function(modulePath) {
-            // Allow @mui and @emotion packages to be transpiled
-            if (/@mui/.test(modulePath) || /@emotion/.test(modulePath)) {
-              return false;
-            }
-            // Use original exclude for everything else
-            if (!originalExclude) {
-              return false;
-            }
-            if (typeof originalExclude === 'function') {
-              return originalExclude(modulePath);
-            }
-            if (originalExclude.test) {
-              return originalExclude.test(modulePath);
-            }
-            return false;
-          };
-        }
+        // Add a new rule for @mui and @emotion packages before the file-loader
+        oneOfRule.oneOf.unshift({
+          test: /\.(js|mjs|jsx)$/,
+          include: [
+            path.resolve(__dirname, 'node_modules/@mui'),
+            path.resolve(__dirname, 'node_modules/@emotion'),
+            path.resolve(__dirname, 'node_modules/@babel/runtime')
+          ],
+          use: {
+            loader: require.resolve('babel-loader'),
+            options: {
+              customize: require.resolve(
+                'babel-preset-react-app/webpack-overrides'
+              ),
+              presets: [
+                [
+                  require.resolve('babel-preset-react-app'),
+                  {
+                    runtime: 'automatic',
+                  },
+                ],
+              ],
+              plugins: [
+                '@babel/plugin-proposal-optional-chaining',
+                '@babel/plugin-proposal-nullish-coalescing-operator',
+                '@babel/plugin-proposal-logical-assignment-operators',
+                ['@babel/plugin-proposal-class-properties', { loose: true }],
+                ['@babel/plugin-proposal-private-methods', { loose: true }],
+                ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+        });
       }
 
       return webpackConfig;
